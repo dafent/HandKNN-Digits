b8a589ec005d6386a797b40ccbdab4361b1d69d7
Nikita Kostin <n.kostin@partner.samsung.com>
2018-10-25 12:31:02 +0300

    Modified Digits.py

--- Digits.py
+++ Digits.py
@@ -14,19 +14,18 @@ def read_idx(filename):
         shape = tuple(st.unpack('>I', f.read(4))[0] for d in range(dims))
         return np.fromstring(f.read(), dtype = np.uint8).reshape(shape)
         
-raw_train = read_idx("train-images.idx3-ubyte")
+raw_train = read_idx("train-images-idx3-ubyte")
 train_data = np.reshape(raw_train, (60000, 28*28))
-train_label = read_idx("train-labels.idx1-ubyte")
+train_label = read_idx("train-labels-idx1-ubyte")
 
-raw_test = read_idx("t10k-images.idx3-ubyte")
+raw_test = read_idx("t10k-images-idx3-ubyte")
 test_data = np.reshape(raw_test, (10000, 28*28))
-test_label = read_idx("t10k-labels.idx1-ubyte")
+test_label = read_idx("t10k-labels-idx1-ubyte")
 
 X = train_data
 Y = train_label
-X_test = test_data[:100]
-Y_true = test_label[:100]
-Y_true[:10]
+X_test = test_data[:20]
+Y_true = test_label[:20]
 
 class My_knn:
     
@@ -39,25 +38,23 @@ class My_knn:
         
     def predict(self, test_x):
         pred_y = []
-        dist_kmin = []
-        dist = []
         pred_y_counter = []
         count = 0
         for elem in test_x:
+            dist = []
             for ind, x in enumerate(self.train_x):
                 dist.append((My_knn.distance(elem, x), ind))
             dist_kmin = sorted(dist, key = lambda x: x[0])[:self.k]
             pred_y_count = Counter(self.train_y[[item[1] for item in dist_kmin]])
             pred_y.append(pred_y_count.most_common(1)[0][0])
             count += 1
-            print("count: {}, pred_y: {}".format(count, pred_y))
+            print("count: {}, pred_y: {}".format(count, pred_y_count.most_common(1)[0][0]))
             #if count%100 == 0:
                 #print("count: {}, pred_y: {}".format(count, pred_y[count]))
         return pred_y
     
     @staticmethod
     def distance(im_1, im_2):
-        dist = 0.
         p = 100.
         im_1 = np.array(im_1)
         im_2 = np.array(im_2)
@@ -70,9 +67,9 @@ class My_knn:
         #dist = np.sum(im_1*np.log(im_1/im_2))
         return dist
         
-KNN = My_knn(3)
+KNN = My_knn(20)
 
 KNN.fit(X, Y)
 
 Y_pred = KNN.predict(X_test)
-classification_report(Y_true, Y_pred)
+print(classification_report(Y_true, Y_pred))
